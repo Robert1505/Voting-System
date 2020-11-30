e31aba933bb9589705b03a6e4dfd9804b4a70d97
Roby1505 <patainearobertpp@gmail.com>
2020-11-30 15:56:52 +0200

    first commit

---
+++ src/PreparingPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Title from './Components/Title'
+
+export default function PreparingPage(props) {
+    return (
+        <div>
+            <Title value = "Voting System" />
+        </div>
+    )
+}
